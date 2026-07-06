c3749094aad8d57df9bc1640572b3b753ede82b0
Talhah Zahid <talhazahid415@gmail.com>
2026-07-06 22:27:46 +0500

    vercel changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,6 +6,7 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   dialectOptions: {
+    
     ssl: {
       require: true,
       rejectUnauthorized: false,
